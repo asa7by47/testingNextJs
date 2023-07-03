4f6f1d697e57c8110f99dcaa4ff425c79d1ebdca
asa7by47 <omartamergado@gmail.com>
2023-07-03 21:51:29 +0300

    updating media queries

--- pages/projects/[id].jsx
+++ pages/projects/[id].jsx
@@ -74,16 +74,7 @@ const ScrollSpyNav = () => {
           </li>
         </ul>
       </nav>
-      <div
-        data-bs-spy="scroll"
-        data-bs-target="#navbar-example2"
-        data-bs-root-margin="0px 0px -40%"
-        data-bs-smooth-scroll="true"
-        className="scrollspy-example bg-body-tertiary p-3 rounded-2"
-        tabIndex="0"
-      >
-        
-      </div>
+      
     </>
   );
 };
